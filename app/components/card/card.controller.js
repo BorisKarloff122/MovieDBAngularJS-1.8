1a77052ed976d55bc85ec92c4516eccc41b04e1e
BorisKarlof <boriskarlof122@gmail.com>
2021-02-19 18:47:20 +0200

    added modal controllers, added events broadcasting beteween modal and cards controllers

--- app/components/card/card.controller.js
+++ app/components/card/card.controller.js
@@ -3,39 +3,83 @@ const cards = angular.module('cards', []);
 cards.component('card', {
     templateUrl: 'components/card/card.component.html',
     bindings:{
-        movies:'='
+        'movies':'='
     },
     controller: cardController
 });
 
 
-cards.factory('dataService', function () {
+cards.service('dataService', function () {
     return {
-        movie: {}
+        movie: {},
+        movies: {}
     };
 });
 
 cards.component('modalBlock', {
     templateUrl: 'components/modal/modal.component.html',
-    controller: modalCtrl,
+    bindings:{
+         'movies':'=',
+         'ouifm': '@'
+    },
+    controller: modalCtrl
 });
 
-
-
-
-function cardController($scope, dataService) {
+function cardController($scope,$log, dataService) {
     $scope.dataService = dataService;
     $scope.details = function () {
-
         $scope.dataService.movie = this.movie;
+        $scope.$broadcast('movieChange', dataService.movie);
     };
-
-    console.log($scope.dataService.movie);
 }
 
 function modalCtrl($scope, dataService) {
+    $scope.dataService = dataService;
+    $scope.openModal = false;
+    $scope.favor = 'Add to favorites';
+    $scope.movies = {};
+
+    this.$onInit = function (){
+        $scope.movies = this.movies;
+    };
+
+    $scope.$on('movieChange', function (event, data) {
+        $scope.movie = data;
+        $scope.openModal = true;
+        $scope.fire();
+    });
+
+    $scope.fire = function(){
+        $scope.dataService.movies = this.movies;
+        $scope.$broadcast('moviesChange', $scope.dataService.movies);
+    };
+
+
+    $scope.close = function () {
+      $scope.openModal = false;
+    };
 
 }
+cards.controller('switchController', function ($scope) {
+
+    $scope.$on('moviesChange', function (event, data) {
+        $scope.findIndex(data);
+    });
+
+    $scope.findIndex = function(data){
+        alert(data);
+    };
+
+    $scope.nextMovie = function () {
+        alert('next');
+    };
+
+    $scope.previousMovie = function () {
+        alert('prev');
+    };
+
+});
+
 
 cards.filter('imgTemplate', function () {
     return function (text) {
